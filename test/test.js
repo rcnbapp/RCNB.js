15a48ca66b40d46e93f924ea57d6f01dc28a244b
Coxxs <Coxxs@users.noreply.github.com>
2019-02-03 18:00:42 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
 const assert = require('assert')
 const rcnb = require('../rcnb')
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should encode', function() {
-      assert.equal(rcnb.encode(new Uint8Array([114, 99, 110, 98])), 'ɌcńƁȓČņÞ')
-	  assert.equal(rcnb.encode(new TextEncoder('utf-8').encode('Who NB?')), 'ȐȼŃƅȓčƞÞƦȻƝƃŖć')
-    });
-    it('should decode', function() {
-      assert.deepEqual(rcnb.decode('ɌcńƁȓČņÞ'), new Uint8Array([114, 99, 110, 98]))
-      assert.equal(new TextDecoder("utf-8").decode(rcnb.decode('ȐĉņþƦȻƝƃŔć')), 'RCNB!')
-    });
+describe('RCNB', function() {
+  it('should encode', function() {
+    assert.strictEqual(rcnb.encode(new Uint8Array([114, 99, 110, 98])), 'ɌcńƁȓČņÞ')
+    assert.strictEqual(rcnb.encode(new TextEncoder('utf-8').encode('Who NB?')), 'ȐȼŃƅȓčƞÞƦȻƝƃŖć')
   });
+  
+  it('should decode', function() {
+    assert.deepStrictEqual(rcnb.decode('ɌcńƁȓČņÞ'), new Uint8Array([114, 99, 110, 98]))
+    assert.strictEqual(new TextDecoder("utf-8").decode(rcnb.decode('ȐĉņþƦȻƝƃŔć')), 'RCNB!')
+  });
+  
 });
