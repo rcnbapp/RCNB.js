f65358954e6e50485cdcfdbf1ec958380e3be7b2
MomoCow <taocheng@qnap.com>
2019-06-28 14:47:32 +0800

    Add type def

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import { Transform, TransformOptions } from 'stream'
+
 /**
  * Encode a string into rcnb-encoded
  * @param arr The original string/buffer, converted into Uint8Array
@@ -9,3 +11,18 @@ export declare function encode(arr: Uint8Array): string
  * @param str The rcnb-encoded string
  */
 export declare function decode(str: string): Uint8Array
+
+declare class EncodeStream extends Transform { }
+declare class DecodeStream extends Transform { }
+
+/**
+ * Encode input streams into rcnb-encoded streams
+ * @param options Transform options
+ */
+export declare function encodeStream(options: TransformOptions): EncodeStream
+
+/**
+ * Decode rcnb-encoded streams
+ * @param options Transform options
+ */
+export declare function decodeStream(options: TransformOptions): DecodeStream
