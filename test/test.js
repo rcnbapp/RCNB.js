64939df357ac3c15037b066da8455eecdb7893e2
MomoCow <taocheng@qnap.com>
2019-06-28 14:02:23 +0800

    Add tests for Stream API

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 const assert = require('assert')
+const {createReadStream} = require('streamifier')
+const streamEqual = require('stream-equal')
 const rcnb = require('../rcnb')
 
 describe('RCNB', function() {
@@ -14,7 +16,71 @@ describe('RCNB', function() {
     assert.strictEqual(new TextDecoder("utf-8").decode(rcnb.decode('ȐĉņþƦȻƝƃŔć')), 'RCNB!')
   });
 
-  it('should error', function() {
+  it('should encode from stream', async function() {
+    var promises = []
+    var src1 = createReadStream(Buffer.of(114, 99, 110, 98)).pipe(rcnb.encodeStream())
+    var expected1 = createReadStream('ɌcńƁȓČņÞ')
+    promises.push(streamEqual(src1, expected1))
+    var src2 = createReadStream(Buffer.of(222, 233, 111, 122, 222)).pipe(rcnb.encodeStream())
+    var expected2 = createReadStream('ȵßȑƈȓƇńÞƞƃ')
+    promises.push(streamEqual(src2, expected2))
+    var src3 = createReadStream(Buffer.from(new TextEncoder('utf-8').encode('Who NB?'))).pipe(rcnb.encodeStream())
+    var expected3 = createReadStream('ȐȼŃƅȓčƞÞƦȻƝƃŖć')
+    promises.push(streamEqual(src3, expected3))
+
+    var results
+    var timer
+    var timerPromise = new Promise(function(_, reject) {
+      timer = setImmediate(function() {
+        reject(new Error('encoding timeout'))
+      })
+    })
+    try {
+      results = await Promise.race([
+        Promise.all(promises),
+        timerPromise
+      ])
+    } catch (err) {
+      assert.strict.fail(err)
+      return
+    }
+    clearImmediate(timer)
+    assert.strict(results.every(equal => equal === true))
+  });
+  
+  it('should decode from stream', async function() {
+    var promises = []
+    var src1 = createReadStream('ɌcńƁȓČņÞ').pipe(rcnb.decodeStream())
+    var expected1 = createReadStream(Buffer.of(114, 99, 110, 98))
+    promises.push(streamEqual(src1, expected1))
+    var src2 = createReadStream('ȵßȑƈȓƇńÞƞƃ').pipe(rcnb.decodeStream())
+    var expected2 = createReadStream(Buffer.of(222, 233, 111, 122, 222))
+    promises.push(streamEqual(src2, expected2))
+    var src3 = createReadStream('ȐĉņþƦȻƝƃŔć').pipe(rcnb.decodeStream())
+    var expected3 = createReadStream(Buffer.from(new TextEncoder("utf-8").encode('RCNB!')))
+    promises.push(streamEqual(src3, expected3))
+
+    var results
+    var timer
+    var timerPromise = new Promise(function(_, reject) {
+      timer = setImmediate(function() {
+        reject(new Error('encoding timeout'))
+      })
+    })
+    try {
+      results = await Promise.race([
+        Promise.all(promises),
+        timerPromise
+      ])
+    } catch (err) {
+      assert.strict.fail(err)
+      return
+    }
+    clearImmediate(timer)
+    assert.strict(results.every(equal => equal === true))
+  });
+
+  it('should error', async function() {
     // length & 1 == true
     assert.throws(() => rcnb.decode('ɌcńƁȓČņ'))
 
@@ -26,6 +92,16 @@ describe('RCNB', function() {
     assert.throws(() => rcnb.decode('ɍȼȵþ'))
     assert.throws(() => rcnb.decode('ɍȼ'))
     assert.throws(() => rcnb.decode('ȵþ'))
+
+    // non Buffer or string stream
+    var promises = []
+    promises.push(new Promise(function(resolve) {
+      createReadStream({}).pipe(rcnb.encodeStream()).on('error', resolve)
+    }))
+    promises.push(new Promise(function(resolve) {
+      createReadStream({}).pipe(rcnb.decodeStream()).on('error', resolve)
+    }))
+    var results = await Promise.all(promises)
+    assert.strict(results.every(err => err instanceof Error))
   });
-  
 });
